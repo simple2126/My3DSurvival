e999ad3b51b0ae7679873235981307c21a04a101
simple2126 <kimcw212@gmail.com>
2024-10-29 17:20:20 +0900

    동적 환경 조사, 아이템 데이터 구현

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,10 +1,13 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     public PlayerController controller;
     public PlayerCondition condition;
 
+    public ItemData itemData;
+    public Action addItem;
     private void Awake()
     {
         CharacterManager.Instance.Player = this;
